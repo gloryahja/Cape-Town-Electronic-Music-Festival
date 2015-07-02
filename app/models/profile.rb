7d7db444e9d4e46813589c60820685498d4da570
Gloryah <gja2nd@virginia.edu>
2015-07-02 16:43:19 +0200

    Added social media icons + working on soundcloud

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,4 +3,7 @@ class Profile < ActiveRecord::Base
 	belongs_to :user
 	 has_attached_file :avatar, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#" }
 	  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
+	  has_attached_file :cover_photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  		validates_attachment_content_type :cover_photo, :content_type => /\Aimage\/.*\Z/
 end
