6b3a4a4e80c145d03891e42fea53afef7e75636b
stanleyzuo <sjrzuo@gmail.com>
2015-07-09 12:30:53 +0200

    Functional

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations', passwords: 'users/passwords' }
+  devise_for :users
 
-  
   get 'welcome/index'
 
   resources :profiles
@@ -9,21 +8,4 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-class Users::RegistrationsController < Devise::RegistrationsController
- protected
-   def after_sign_up_path_for(resource)
-     @result = Profile.where(:user_id => current_user.id).first
-
-     if(@result.present?)
-       request.env['omniauth.origin'] || stored_location_for(resource) || profile_path(@result.id)
-     else
-       request.env['omniauth.origin'] || stored_location_for(resource) || new_profile_path
-     end
-   end
-
-   def after_update_path_for(resource)
-     signed_in_root_path(resource)
-   end
- end 
-  
 end
